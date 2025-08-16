8e859861675eb439ff5f8918c4f75a3185765a29
JaeWoo Joe <joejw7554@gmail.com>
2025-08-16 09:39:12 +0900

    Practicing Dijkstra

--- AlgorithmPractice/Search/Dijkstra/HighwayShortcut.cs
+++ AlgorithmPractice/Search/Dijkstra/HighwayShortcut.cs
@@ -1,93 +1,52 @@
 using System;
 using System.Collections.Generic;
 
-public class HighwayShortcut
+//https://www.acmicpc.net/problem/1446
+public class ShortestPath
 {
-    // 지름길 정보를 담는 클래스
-    public class Shortcut
+    // 노드를 표현하는 클래스 (거리, 위치)
+    public class Node
     {
-        public int Start { get; set; }
-        public int End { get; set; }
-        public int Length { get; set; }
-        public Shortcut(int start, int end, int length)
-        {
-            Start = start;
-            End = end;
-            Length = length;
-        }
+        public int Distance { get; set; }
+        public int Position { get; set; }
     }
 
-    // 입력값
-    static int N; // 지름길 개수
-    static int D; // 고속도로 길이
-    static List<Shortcut> Shortcuts = new();
-
-    // 최단 거리 계산 함수 (알고리즘은 직접 구현)
-    static void HandleInput()
+    public static void Main(string[] args)
     {
-        int[] token;
-        token = Array.ConvertAll(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries), int.Parse);
+        int N, D;
+        string[] firstLine = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries & StringSplitOptions.TrimEntries);
+        N = int.Parse(firstLine[0]);
+        D = int.Parse(firstLine[1]);
 
-        N = token[0];
-        D = token[1];
+        List<Node>[] road = new List<Node>[D + 1];
 
+        // 일반 도로 간선 추가
+        for (int i = 0; i < D; i++)
+        {
+            Node regularRoad = new Node();
+            regularRoad.Distance = 1;
+            regularRoad.Position = i + 1;
+            road[i].Add(regularRoad);
+        }
+
+        // 지름길 추가 (기존 코드 개선)
         for (int i = 0; i < N; i++)
         {
-            int[] shortcutInfo = Array.ConvertAll(
-                Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries), int.Parse);
-            Shortcuts.Add(new Shortcut(shortcutInfo[0], shortcutInfo[1], shortcutInfo[2]));
+            int[] shortcuts = Array.ConvertAll(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries), int.Parse);
+            int start = shortcuts[0];
+            int end = shortcuts[1];
+            int distance = shortcuts[2];
 
-        }
-    }
-
-
-    static int GetMinimumDistance()
-    {
-        int[] dist = new int[D + 1];
-        for (int i = 0; i <= D; i++)
-            dist[i] = int.MaxValue;
-        dist[0] = 0;
-
-        // 우선순위 큐: (현재 위치, 현재까지의 거리)
-        PriorityQueue<int, int> pq = new();
-        pq.Enqueue(0, 0);
-
-        while (pq.Count > 0)
-        {
-            pq.TryDequeue(out int curPos, out int curDist);
-
-            // 이미 더 짧은 경로로 방문했다면 스킵
-            if (dist[curPos] < curDist)
-                continue;
-
-            // 일반 도로로 한 칸 이동
-            if (curPos + 1 <= D && dist[curPos + 1] > dist[curPos] + 1)
+            if (end <= D && 0 <= start && start < D && distance < (end - start))
             {
-                dist[curPos + 1] = dist[curPos] + 1;
-                pq.Enqueue(curPos + 1, dist[curPos + 1]);
-            }
-
-            // 현재 위치에서 시작하는 지름길 모두 확인
-            foreach (var shortcut in Shortcuts)
-            {
-                if (shortcut.Start == curPos && shortcut.End <= D)
-                {
-                    if (dist[shortcut.End] > dist[curPos] + shortcut.Length)
-                    {
-                        dist[shortcut.End] = dist[curPos] + shortcut.Length;
-                        pq.Enqueue(shortcut.End, dist[shortcut.End]);
-                    }
-                }
+                Node node = new Node();
+                node.Distance = distance;
+                node.Position = end;
+                road[start].Add(node);
             }
         }
 
-        return dist[D];
+
     }
 
-    static void Main()
-    {
-        HandleInput();
-
-        GetMinimumDistance();
-    }
 }
